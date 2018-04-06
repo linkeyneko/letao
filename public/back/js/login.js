6c272ba2f079b1869d5c4fedbfb0d1a97c6d17a7
linkey <852532529@qq.com>
2018-04-06 20:39:25 +0800

    登录功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -11,7 +11,8 @@ $(function(){
     fields: {
       username: {
         validators: {
-          notEmpty: {message: "用户名不能为空"}
+          notEmpty: {message: "用户名不能为空"},
+          callback:{message:"用户名错误"}
         }
       },
       password: {
@@ -21,9 +22,40 @@ $(function(){
             min: 6,
             max: 12,
             message: "密码长度必须是6-12位"
-          }
+          },
+          callback:{message:"密码错误"}
         }
       }
     }
+  })
+  $("#form").on("success.form.bv",function(e){
+    e.preventDefault();
+    var formdata=$(this).serialize();
+    $.ajax({
+      type:'post',
+      url:'/employee/employeeLogin',
+      dataType:'json',
+      data:formdata,
+      success:function(info){
+        if(info.error==1000){
+          $("#form").data("bootstrapValidator").updateStatus("username","INVALID","callback");
+        }else if(info.error==1001){
+          $("#form").data("bootstrapValidator").updateStatus("password","INVALID","callback");
+        }else if(info.success){
+          location.href="/back/index.html";
+        }
+      }
+    })
+  })
+  $("[type='reset']").click(function(){
+    $("#form").data("bootstrapValidator").resetForm();
+  })
+  $(document).ajaxStart(function(){
+    NProgress.start();
+  })
+  $(document).ajaxStop(function(){
+    setTimeout(function(){
+      NProgress.done();
+    },500)
   })
 })
